f7d718501863e65e6a3b23a693fee5e9632f4ade
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-05 23:50:38 -0400

    se agrega modulo text to speech

---
+++ text-to-speech.py
@@ -0,0 +1,25 @@
+'''
+This Python project uses a range of new libraries to convert an existing article into a playable mp3 file. You’ll need to install nltk (natural language toolkit), newspaper3k, and gtts (see pip install instructions).
+'''
+
+import nltk
+from newspaper import Article
+from gtts import gTTS
+
+
+def text_to_speech(url):
+   article = Article(url)
+   article.download()
+   article.parse()
+   nltk.download('punkt')
+   article.nlp()
+   article_text = article.text
+   language = 'en'
+   my_obj = gTTS(text=article_text, lang=language, slow=False)
+   my_obj.save("read_article.mp3")
+
+
+if __name__ == '__main__':
+   text_to_speech(
+       url='https://hackr.io/blog/top-tech-companies-hiring-python-developers'
+   )
